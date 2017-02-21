b742df039c37c36f5f81651091ac2fb0ef80d4da
Bằng Ngô <Bng Ng>
2017-02-21 16:38:58 +0700

    Lesson 11 Stuctural directives

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -4,12 +4,22 @@ import { Component } from '@angular/core';
     selector: 'my-tutorial',
     template: `
     <h2>{{title}}</h2>
-    <input type="text" [(ngModel)] = "fname"/>
-    <input type="text" [(ngModel)] = "lname"/>
-    <br>
-    Full name: {{fname}} {{lname}}
+    <h3 *ngIf="showLineIf">This ngIf directive line.</h3>
+    <div [ngSwitch]="color">
+        <p *ngSwitchCase="'red'">This is line color is red</p>
+        <p *ngSwitchCase="'blue'">This is line color is blue</p>
+        <p *ngSwitchCase="'green'">This is line color is green</p>
+        <p *ngSwitchDefault>Invalid color</p>
+    </div>
+    <ul>
+    <li *ngFor="let color of colors">{{color}}</li>
+    </ul>
     `
 })
 export class TutorialComponent {
-    public title = "This is Tedu Angular2 Tutorial compponent";
+    public title = 'This is Tedu Angular2 Tutorial compponent';
+    public showLineIf = false;
+    public color = 'red';
+    public colors:string[] = ["red","green","blue"];
+    // tslint:disable-next-line:eofline
 }
