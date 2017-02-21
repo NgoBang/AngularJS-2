0d5574920a5febfdb4ae4f13c75623aaa150af70
Bằng Ngô <Bng Ng>
2017-02-21 11:24:35 +0700

    commit lesson 8

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
-    template: `<h2>This is Tedu Angular2 Tutorial</h2>`
+    template: `
+    <h2>This is Tedu Angular2 Tutorial</h2>
+    <h3 [class.redColor]="applyClass">Apply class</h3>
+    <h1 [class.changeSize] = "applySize">This is size Text</h1>
+    <h4 [style.color]="blueColor?'blue':'orange'">Apply style</h4>
+    <p [style.font-size]="font?'160%':'100%'">Kích thước phông</p>
+    `,
+    styles: [`.redColor{
+        color:red;
+    }
+    .changeSize{
+        font-size:150%;
+    }
+    `]
 })
 export class TutorialComponent {
-
+    public applyClass = true;
+    public applySize = true;
+    public blueColor = false;
+    public font = false;
 }
