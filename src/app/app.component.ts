b515e9fd76dd0cc4b8d8d0029bb345bcd9182ca6
Bằng Ngô <Bng Ng>
2017-02-22 22:00:11 +0700

    Lesson 13 Tương tác giữa các component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {TutorialComponent} from './tutorial.component';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Hello {{name}}</h1>
-  <my-tutorial></my-tutorial>
+  <h1>Hello {{title}}</h1>
+  <input type="text" #textName (keyup)="0"/>
+  <p>Agree number: {{agree}}. Disgree: {{disgree}}</p>
+  <button (click)="changeName()">Change name</button>
+  <my-tutorial *ngFor="let person of names" [name]="person" (onVote)="parentVote($event)"></my-tutorial>
   `,
 })
 export class AppComponent {
-  name = 'Bang';
+  public title = 'Bang';
+  public agree: number = 0;
+  public disgree: number = 0;
+  public names = ['Mr A', 'Mr B', 'Mr C', 'Mr D'];
+  
+  @ViewChild(TutorialComponent)
+  private tutorialComponent: TutorialComponent;
+  parentVote(agree: boolean) {
+    if (agree) this.agree++;
+    else this.disgree++;
+  }
+  changeName(){
+      this.tutorialComponent.setName('Change name in Parent');
+  }
 }
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,32 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
     template: `
     <h2>{{title}}</h2>
-    <p [ngClass]="{classOne:cone, classTwo:ctwo}">This is ngClass apply style</p>
-    <button (click)="toggle()">Toggle</button>
-    <p [ngStyle]="{'font-style':style, 'font-size':size}">This pragraph will be apply to ngStyle</p>
-    `,
-    styles: [
-        `
-        .classOne{
-            color: white;
-        }
-        .classTwo{
-            background-color:black;
-        }
-        `
-    ]
+    <p>Child component: {{name}}</p>
+
+    <button [disabled]="voted" (click)="vote(true)">Agree</button>
+    <button [disabled]="voted" (click)="vote(false)">Disgree</button>
+    <br/>
+    Result: {{voted}}
+    `
 })
 export class TutorialComponent {
-    public cone: boolean = true;
-    public ctwo = true;
-    toggle() {
-        this.cone = !this.cone;
-        this.ctwo = !this.ctwo;
+    @Input() name: string;
+    
+    @Output() onVote = new EventEmitter<boolean>();
+
+    public voted: boolean = false;
+    
+    setName(name:string){
+        this.name = name;
     }
-    public style = "italic";
-    public size = "30px";
+    vote(agree: boolean) {
+        this.voted = true;
+        this.onVote.emit(agree);
+    }
     // tslint:disable-next-line:eofline
 }
