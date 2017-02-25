e418369223ea53e8028d1370e77de67758a245ac
Bằng Ngô <Bng Ng>
2017-02-25 13:31:14 +0700

    Bài 15: Xây dựng Form từ template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>Hello {{title | uppercase}}!</h1>
-  <p>Date today: {{today| date:'shortDate' | uppercase}}</p>
-  <p>Percent: {{percentNumber| percent:'4.3-5'}}</p>
-  <p>Number: {{e | number:'3.1-5'}}</p>
-  <p>{{obj | json}}
-  <p>2 power 10 {{ 2 |  exponentialStrength:10}}</p>
-  <ul>
-    <li *ngFor="let i of collection | slice:1:3">{{i}}</li>
-  </ul>
-  <my-tutorial></my-tutorial>
-  `,
+  templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
-  title = 'Bang';
-  public today = Date.now();
-  public percentNumber = 1.3495;
-  public e: number = 2.7123213876786;
-  public obj: Object = {
-    foo: 'bar',
-    baz: 'qux',
-    nested: {
-      xyz: 3,
-      numbers: [1, 2, 3, 4, 5, 6, 7]
-    }
-  };
-  public collection: string[] = ['a', 'b', 'c', 'd'];
+  onSubmit(value:any){
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TutorialComponent } from './tutorial.component';
 import { FormsModule } from '@angular/forms';
-import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, TutorialComponent, ExponentialStrengthPipe],
+  declarations: [AppComponent, TutorialComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
